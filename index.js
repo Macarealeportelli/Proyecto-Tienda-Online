520af850a51e90984ccfc038a2eaa419a12cfc0a
Macarealeportelli <macarenarealeportelli@gmail.com>
2020-10-17 20:48:26 -0300

    agrego funcion de botones grilla y lista

--- index.js
+++ index.js
@@ -11,13 +11,49 @@ cerrarCarrito.onclick =()=>{
     menu.classList.add("hidden");
 }
 
-//CAMBIO PRODUCTOS DE GUILLA A COLUMNA
+//CAMBIO PRODUCTOS DE GUILLA A LISTA 
 const botonGrilla = document.getElementById("boton-grilla")
 const botonLista = document.getElementById("boton-lista")
 const tarjetas = document.querySelectorAll(".tarjeta")
-const descripcionProducto = document.querySelectorAll("#descripcion-producto")
+const descripcionesProductos = document.querySelectorAll("#descripcion-producto")
+const contenidosProductos = document.querySelectorAll("#contenido")
+// console.log(botonGrilla)
+// console.log(botonLista)
+// console.log(tarjetas)
+// console.log(descripcionProducto)
 
-console.log(botonGrilla)
-console.log(botonLista)
-console.log(tarjetas)
-console.log(descripcionProducto)
+//DE GRILLA A LISTA
+botonLista.onclick=()=>{
+    for (let tarjeta of tarjetas){
+        tarjeta.classList.remove("grilla")
+        tarjeta.classList.add("lista")
+       
+        for(let contenido of contenidosProductos){
+            contenido.classList.remove("contenido-poducto-grilla")
+            contenido.classList.add("contenido-producto-lista")
+        }
+
+        for(let descripcion of descripcionesProductos){
+            descripcion.classList.remove("hidden")
+        }
+        
+    }
+}
+
+//DE LISTA A GRILLA
+botonGrilla.onclick=()=>{
+    for (let tarjeta of tarjetas){
+        tarjeta.classList.remove("lista")
+        tarjeta.classList.add("grilla")
+       
+        for(let contenido of contenidosProductos){
+            contenido.classList.remove("contenido-poducto-lista")
+            contenido.classList.add("contenido-producto-grilla")
+        }
+
+        for(let descripcion of descripcionesProductos){
+            descripcion.classList.add("hidden")
+        }
+        
+    }
+}
